233c488af4c06059404f6bf1893770d4384dfca0
ethanstrominger <ethanstrominger2@gmail.com>
2021-07-07 20:21:30 -0400

    Read from .env
    Co-authored-by: Terence Stone <terence.stone25@gmail.com>
    Co-authored-by: Thad Kerosky <contact@thadk.net>

--- src/http-common.js
+++ src/http-common.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:5000/";
+export const baseURL = process.env.REACT_APP_BACKEND_API;
+//export const baseURL = "http://localhost:5000";
+console.log("BaseURL ", baseURL);
 
 export default axios.create({
   baseURL,
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
-
