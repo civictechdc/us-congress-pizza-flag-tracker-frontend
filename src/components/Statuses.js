6d65037c5e9a7b4c8f5caca396fba11efec1788f
Merritt Baggett <mertbagt@gmail.com>
2021-09-15 20:19:26 -0400

    combined items 2 and 3

--- src/components/Statuses.js
+++ src/components/Statuses.js
@@ -10,38 +10,33 @@ export const STATUSES = [{
   "description": "HOSS verified as printed and scanned physical QR code"
 }, {
   "id": 3,
-  "office_code": "HOSS",
+  "office_code": "AOC flag office",
   "sequence_num": 3,
-  "description": "HOSS sent flag to AOC" 
+  "description": "AOC received flag from HOSS"
 }, {
   "id": 4,
   "office_code": "AOC flag office",
   "sequence_num": 4,
-  "description": "AOC received flag from HOSS"
-}, {
-  "id": 5,
-  "office_code": "AOC flag office",
-  "sequence_num": 5,
   "description": "AOC has flown flag and added certificate"
     // OPTIONAL: date specified vs soon as possible
 }, {
-  "id": 6,
+  "id": 5,
   "office_code": "Mail Services",
-  "sequence_num": 6,
+  "sequence_num": 5,
   "description": "Mail Services picked up flag from AOC"
+}, {
+  "id": 6,
+  "office_code": "STATE",
+  "sequence_num": 6,
+  "description": "Congressional Staffer received flag"
 }, {
   "id": 7,
   "office_code": "STATE",
   "sequence_num": 7,
-  "description": "Congressional Staffer received flag"
+  "description": "Staffer order closed (Flag ready for shipment)"
 }, {
   "id": 8,
   "office_code": "STATE",
   "sequence_num": 8,
-  "description": "Staffer order closed (Flag ready for shipment)"
-}, {
-  "id": 9,
-  "office_code": "STATE",
-  "sequence_num": 9,
   "description": "Constituent notified"
 }]
