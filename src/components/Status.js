737c7be0bb9305425c784a0454eea9d0aa10bb94
Merritt Baggett <mertbagt@gmail.com>
2021-09-21 17:41:16 -0400

    hide status select if not logged in

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+
 import { STATUSES, DEPTCODES } from "./Statuses.js";
 
 const Status = () => {
   const initialStatusState = {
     id: null,
     login_office_code: "not logged in",
-    current_desciption: "",
+    current_desciption: "select a status",
   };
   const [status, setStatus] = useState(initialStatusState);
 
@@ -14,7 +15,37 @@ const Status = () => {
     setStatus({ ...status, [name]: value });
   };
 
-  return (
+  const unLogged = (status.login_office_code === "not logged in")
+    ? ""
+    : <div>
+        <h6>Status: {status.current_desciption}</h6>
+          <form>
+            <div className="form-group">
+              <label htmlFor="current_desciption">Log in: </label>
+              <select
+                value={status.current_desciption}
+                id="current_desciption"
+                onChange={handleInputChange}
+                name="current_desciption"
+                >
+                {STATUSES && STATUSES.map((element, index) => {
+                  if ((status.login_office_code === element.office_code) || 
+                  (status.login_office_code === "ADMIN") || 
+                  ((element.office_code === "ALL") && (status.login_office_code !== "not logged in"))
+                  ) {
+                    return (
+                      <option value={element.description} key={index}>
+                        #{element.sequence_num} {element.description}
+                      </option>              
+                    );
+                  }
+                })}
+              </select>
+            </div>      
+          </form>
+      </div>;
+
+   return (
     <div>
       <h4>User: {status.login_office_code}</h4>
       <form>
@@ -37,31 +68,15 @@ const Status = () => {
         </div>
         <hr />
       </form>
-      <h6>Status: {status.current_desciption}</h6>
-      <form>
-        <div className="form-group">
-          <label htmlFor="current_desciption">Log in: </label>
-          <select
-              value={status.current_desciption}
-              id="current_desciption"
-              onChange={handleInputChange}
-              name="current_desciption"
-            >
-            {STATUSES && STATUSES.map((element, index) => {
-              if ((status.login_office_code === element.office_code) || 
-                (status.login_office_code === "ADMIN") || 
-                ((element.office_code === "ALL") && (status.login_office_code !== "not logged in"))
-                ) {
-                  return (
-                    <option value={element.description} key={index}>
-                      #{element.sequence_num} {element.description}
-                    </option>              
-                  );
-              }
-            })}
-          </select>
-        </div>
-      </form>
+      
+
+
+
+    {unLogged}
+
+
+
+      
     </div>      
   );
 }
