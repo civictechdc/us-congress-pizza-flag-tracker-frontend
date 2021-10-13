d5c1c1e458fc2d673e1c67c21f0f8fdf6c7391c5
aaronm759 <amcfarlin76@yahoo.com>
2021-10-13 17:44:13 -0400

    error fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import devAuth from "./Authenticate"
 
 import Publicheader from "./publicHeader";
