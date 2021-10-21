516ca00fcf76496fbc29fbc0feaf2a93422d1833
aaronm759 <amcfarlin76@yahoo.com>
2021-10-21 09:41:58 -0400

    removed artifact

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import devAuth from "./Authenticate"
 
 function PrivateRoute ({ children, ...rest }) {
-    console.log('here')
+    
    return (
      <Route {...rest} render={ () => {
          return ( devAuth.isAuthenticated === true
