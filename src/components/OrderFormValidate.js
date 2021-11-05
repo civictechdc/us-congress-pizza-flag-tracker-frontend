aa864113a207a7932f18ad6bb61617def624f81a
Merritt Baggett <mertbagt@gmail.com>
2021-11-05 16:48:42 -0400

    validation message spacing tweak

--- src/components/OrderFormValidate.js
+++ src/components/OrderFormValidate.js
@@ -11,22 +11,22 @@ const OrderFormValidate = (props) => {
       {!order.order_number ? (
         <p>Enter a valid Order Number</p>
       ) : (
-        <p>&nbsp;</p>
+        ""
       )}
       {!order.usa_state ? (
         <p>Pick a US State</p>
       ) : (
-        <p>&nbsp;</p>
+        ""
       )}
       {!order.home_office_code ? (
         <p>Pick a Congressional Office</p>
       ) : (
-        <p>&nbsp;</p>
+        ""
       )}
       {!districtMatchCheck ? (
         <p>US State and Congressional Office must correspond</p>
       ) : (
-        <p>&nbsp;</p>
+        ""
       )}
     </aside>
   );
