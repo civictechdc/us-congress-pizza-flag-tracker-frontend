8f85696b73dad6b982ba782a1b8782eb342f5c5d
Merritt Baggett <mertbagt@gmail.com>
2021-11-23 20:10:25 -0500

    preliminary work for Backend to take over defining Statuses

--- src/components/ScanOrder.js
+++ src/components/ScanOrder.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import OrderDataService from "../services/OrderService";
+import StatusDataService from "../services/StatusService";
 import { STATUSES } from "./Statuses.js";
 
 const ScanOrder = (props) => {
@@ -27,11 +28,15 @@ const ScanOrder = (props) => {
 
   const [order, setOrder] = useState(initialOrderState);
   const [message, setMessage] = useState(initialMessageState);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [popUpBox, setPopUpbox] = useState("none");
+  const loginError = "You must be logged in to view this page";
 
   const getOrder = (id) => {
     OrderDataService.get(id)
       .then((response) => {
         setOrder(response.data);
+        console.log("Resp: ", response.data);
       })
       .catch((e) => {
         console.log(e);
@@ -41,6 +46,32 @@ const ScanOrder = (props) => {
   useEffect(() => {
     getOrder(props.match.params.id);
   }, [props.match.params.id]);
+
+  const retrieveStatuses = () => {
+    StatusDataService.getStatus()
+      .then((response) => {
+        console.log("Statuses: ", response.data);
+        // setOrders(response.data.orders);
+      })
+      .catch((e) => {
+        console.log("Status Error");
+        console.log(e);
+        if (e.response?.status === 401) {
+          setErrorMessage(loginError);
+        } else {
+          setPopUpbox("block");
+          setErrorMessage(
+            e.message +
+              "." +
+              "Check with admin if server is down or try logging out and logging in."
+          );
+        }
+      });
+  };
+
+  useEffect(() => {
+    retrieveStatuses();
+  }, []);
 
   const dynamicSort = (property) => {
     let sortOrder = 1;
@@ -109,74 +140,88 @@ const ScanOrder = (props) => {
       });
   };
 
+  const closePopUpBox = () => {
+    setPopUpbox("none");
+  };
+
   return (
-    <div>
-      {order ? (
-        <>
-          <div className="form-group">
-            <label htmlFor="order_number">
-              Order Number: <strong>{order.order_number}</strong>
-            </label>
-          </div>
-          <div className="form-group">
-            <label htmlFor="usa_state">
-              US State: <strong>{order.usa_state}</strong>
-            </label>
-          </div>
-          <div className="form-group">
-            <label htmlFor="home_office_code">
-              Congressional Office: <strong>{order.home_office_code}</strong>
-            </label>
-          </div>
-          <div className="form-group">
-            <label htmlFor="current_status">
-              Current Status:{" "}
-              {order.status.description ? (
-                <strong>
-                  #{order.status.sequence_num} - {order.status.description}
-                </strong>
-              ) : (
-                <strong>Missing status...</strong>
-              )}
-            </label>
-          </div>
-          {nextDesc === "FINAL" ? (
+    <>
+      <div>
+        {order ? (
+          <>
             <div className="form-group">
-              <label htmlFor="next_status">
-                <strong>Order Complete</strong>
+              <label htmlFor="order_number">
+                Order Number: <strong>{order.order_number}</strong>
               </label>
             </div>
-          ) : (
-            <>
+            <div className="form-group">
+              <label htmlFor="usa_state">
+                US State: <strong>{order.usa_state}</strong>
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="home_office_code">
+                Congressional Office: <strong>{order.home_office_code}</strong>
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="current_status">
+                Current Status:{" "}
+                {order.status.description ? (
+                  <strong>
+                    #{order.status.sequence_num} - {order.status.description}
+                  </strong>
+                ) : (
+                  <strong>Missing status...</strong>
+                )}
+              </label>
+            </div>
+            {nextDesc === "FINAL" ? (
               <div className="form-group">
                 <label htmlFor="next_status">
-                  Next Status:{" "}
-                  {STATUSES && order ? (
-                    <strong>
-                      #{nextSeq} - {nextDesc}
-                    </strong>
-                  ) : (
-                    <strong>Missing data needed to generate next Status</strong>
-                  )}
+                  <strong>Order Complete</strong>
                 </label>
               </div>
-              <button onClick={handleUpdate} className="btn btn-success">
-                {"Update Status"}
-              </button>{" "}
-              <button onClick={updateOrder} className="btn btn-success">
-                {"Save"}
-              </button>
-            </>
-          )}
-          <p>{message.success}</p>
-        </>
-      ) : (
-        <div>
-          <br />
-          <p>Please click on an order...</p>
+            ) : (
+              <>
+                <div className="form-group">
+                  <label htmlFor="next_status">
+                    Next Status:{" "}
+                    {STATUSES && order ? (
+                      <strong>
+                        #{nextSeq} - {nextDesc}
+                      </strong>
+                    ) : (
+                      <strong>
+                        Missing data needed to generate next Status
+                      </strong>
+                    )}
+                  </label>
+                </div>
+                <button onClick={handleUpdate} className="btn btn-success">
+                  {"Update Status"}
+                </button>{" "}
+                <button onClick={updateOrder} className="btn btn-success">
+                  {"Save"}
+                </button>
+              </>
+            )}
+            <p>{message.success}</p>
+          </>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on an order...</p>
+          </div>
+        )}
+      </div>
+
+      <div className="pop-container" style={{ display: popUpBox }}>
+        <div className="pop-up" onClick={closePopUpBox}>
+          <h3>{errorMessage}</h3>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
