f914d5c4002df4cddaf2f14766e7947a30fac43c
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2021-12-18 10:49:26 -0500

    more tests

--- cypress/integration/basic_tests.js
+++ cypress/integration/basic_tests.js
@@ -24,10 +24,60 @@ describe("Loading and logging in", () => {
   });
 });
 
-//   describe("CRUD actions for superuser", ()=>{
-//       it("allows FED-ADMIN to create a new order then update it", ()=>{
-//           cy.request('POST','/login', {username: "FED-ADMIN", password: "FED-ADMIN-1010"})
-//           cy.visit('/add')
-//       }
-//       )
-//   })
+describe("CRUD actions for superuser", () => {
+  beforeEach(() => {
+    cy.request({
+      method: "POST",
+      url: `${Cypress.env("api_server")}/signin`,
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      auth: { username: "FED-ADMIN", password: "FED-ADMIN-1010" },
+    }).then((response) => {
+      cy.setLocalStorage("user", JSON.stringify(response.body));
+    });
+  });
+  it("allows FED-ADMIN to create a new order ", () => {
+    cy.visit("/add");
+    //TODO add a name to the disabled Office input so that we can do cy.get("input[name=congressional_office]").should("be.disabled")
+    cy.get("input[name=order_number]").type("1234567890");
+    cy.get("input[id=react-select-3-input]").type(`CO{enter}`);
+    // cy.get("input[id=react-select-5-input]").click().find('[id*="5-option"]').eq(1).should("equal","CO-01").type("CO-01");
+    //I'd like the above test to work but I'm having trouble getting Cypress to work with the react-selector. As a compromise/stopgap we use the below instead:
+    cy.get("input[id=react-select-5-input]").click().type(`CO-01{enter}`);
+    cy.get(".btn").click();
+    cy.get("h4").contains("You submitted successfully!");
+  });
+  it("does not allow FED-ADMIN to create a new order with a duplicate order number", () => {
+    cy.visit("/add");
+    //TODO add a name to the disabled Office input so that we can do cy.get("input[name=congressional_office]").should("be.disabled")
+    cy.get("input[name=order_number]").type("1234567890");
+    cy.get("input[id=react-select-3-input]").type(`CO{enter}`);
+    // cy.get("input[id=react-select-5-input]").click().find('[id*="5-option"]').eq(1).should("equal","CO-01").type("CO-01");
+    //I'd like the above test to work but I'm having trouble getting Cypress to work with the react-selector. As a compromise/stopgap we use the below instead:
+    cy.get("input[id=react-select-5-input]").click().type(`CO-01{enter}`);
+    cy.get(".btn").click();
+    //response should be 500--or later, a more specific error--not sure how to get response code here though!
+  });
+
+  it("allows FED-ADMIN to edit an order", () => {
+    cy.visit("/orders");
+    cy.get(".flag-group-item").contains("1234567890").click(); //danger, this may break upon implementing new layout if class name changes
+    cy.get(".badge-warning").eq(0).click();
+    cy.get("input[id=react-select-7-input]").click().type(`CO-02{enter}`); //why is it different on the edit screen? where do these numbers in the ids come from?
+    cy.get(".btn-success").click();
+    cy.visit("/orders");
+    cy.get("td")
+      .contains("1234567890")
+      .next()
+      .next()
+      .should("contain", "CO-02");
+  });
+  it("allows FED-ADMIN to delete an order", () => {
+    cy.get(".flag-group-item").contains("1234567890").click(); //danger, this may break upon implementing new layout if class name changes
+    cy.get(".badge-warning").eq(0).click();
+    cy.get(".badge-danger").click(); //there probably should be a confirmation step here both before and after deletion, SO, this test will need to be updated
+    cy.get("main").wait(1000).should("not.contain", "1234567890"); //cypress docs say not to use wait but otherwise it's testing against the empty, pre-rendered table
+  });
+});
