1f5c3c18ca690285cd121b73c4d3e14fbbaa9db8
Merritt Baggett <mertbagt@gmail.com>
2021-12-20 19:46:02 -0500

    removed comments StatusService

--- src/services/StatusService.js
+++ src/services/StatusService.js
@@ -8,15 +8,8 @@ const updateStatus = (id, data) => {
   return httpAuthenticate().put(`/scan/${id}`, data);
 };
 
-/*
-const revertStatus = (id, data) => {
-  return httpAuthenticate().put(`/revert/${id}`, data);
-};
-*/
-
 const statusServiceObject = {
   getStatus,
   updateStatus,
-  // revertStatus,
 };
 export default statusServiceObject;
