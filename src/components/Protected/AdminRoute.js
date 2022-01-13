eff06fef137b78fd2857ce7fb9c2f07012b5d02a
aaronm759 <amcfarlin76@yahoo.com>
2022-01-13 14:57:16 -0500

    remove unused var

--- src/components/Protected/AdminRoute.js
+++ src/components/Protected/AdminRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect, useHistory } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { adminControl } from './Permissions';
 
 
