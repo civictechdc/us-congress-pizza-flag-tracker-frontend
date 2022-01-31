1d465ed800b2f971f193a0fbd642ed395669b4b2
Merritt Baggett <mertbagt@gmail.com>
2022-01-31 09:56:23 -0500

    minor style adjustments

--- src/components/pages/printOrder.js
+++ src/components/pages/printOrder.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { baseURL } from "../../http-common";
+import AuthService from "../../service/authService";
 import OrderDataService from "../../service/orderService";
+import "./printOrder.css";
 
 const PrintOrder = (props) => {
   const initialOrderState = {
@@ -48,12 +50,30 @@ const PrintOrder = (props) => {
   return (
     <>
       <div className="form-group">
-        <label>QR Code</label>
-        {order.uuid}
+        <label htmlFor="order_number">
+          Order Number: <strong>{order.order_number}</strong>
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="usa_state">
+          US State: <strong>{order.usa_state}</strong>
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="home_office_code">
+          Congressional Office: <strong>{order.home_office_code}</strong>
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          QR Code: <strong>{order.uuid}</strong>
+        </label>
+      </div>
+      <div className="form-group">
         <img
           src={baseURL + "/qrcode/" + order.uuid}
           alt="QR Code"
-          align="right"
+          class="center"
         />
       </div>
     </>
