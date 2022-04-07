de30b1c46fbb0723b5805ef79ca9c05d5ec548f7
Merritt Baggett <mertbagt@gmail.com>
2022-04-07 12:25:07 -0400

    removed test text from refuseUpdateButton

--- src/components/scanOrder/buttons/refuseUpdateButton.js
+++ src/components/scanOrder/buttons/refuseUpdateButton.js
@@ -14,7 +14,7 @@ const RefuseUpdateButton = (props) => {
         className="btn btn-success"
         style={{ opacity: 0.6 }}
       >
-        {"Update Status Refuse"}
+        {"Update Status"}
       </button>
     </>
   );
