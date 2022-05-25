4c36becb8568fdc889256f9c7ef5cf95ff3df7ee
Merritt Baggett <mertbagt@gmail.com>
2022-05-25 16:10:17 -0400

    more code clean up

--- src/components/orderForm.js
+++ src/components/orderForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 import { baseURL } from "../http-common";
 import { STATES } from "./states.js";
