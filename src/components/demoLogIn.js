164de30db9c39e281dbb9efa28f2860b5e3ac249
Merritt Baggett <mertbagt@gmail.com>
2022-06-07 13:24:45 -0400

    tweaking redirect

--- src/components/demoLogIn.js
+++ src/components/demoLogIn.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, generatePath } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import PopUpBoxComponent from "./popUpBoxComponent";
 import AuthService from "../service/authService";
 import OrderDataService from "../service/orderService";
@@ -14,38 +15,74 @@ const DemoLogIn = (props) => {
 
   const [message, setMessage] = useState(initialMessageState);
   const [popUpBox, setPopUpBox] = useState("none");
-  const [redirectNow, setRedirectNow] = useState("");
+  const [reload, setReload] = useState("yes");
+  // const [redirectNow, setRedirectNow] = useState("");
+  let redirectNow = "";
+  // let reload = "yes";
+  const history = useHistory();
 
-  const closePopUpBox = () => {
-    setPopUpBox("none");
-    setMessage({
-      ...message,
-      text: "Please wait a moment for the database to reset.",
-    });
-  };
-
-  let params = new URLSearchParams(document.location.search);
-  console.log("q: ", params.get("q"));
+  const params = new URLSearchParams(document.location.search);
   console.log("Loc: ", document.location.pathname);
-  let priorLocation = document.location.pathname.slice(0, -10);
+  const priorLocation = document.location.pathname.slice(0, -10);
   console.log("Loc Editted: ", priorLocation);
 
-  useEffect(() => {
-    // setRedirectNow("yes");
-    AuthService.logout();
-    // window.location.reload();
-  }, [redirectNow]);
+  const userName = params.get("q");
+  console.log("Username: ", userName);
+  const password = userName + "-1010";
+  console.log("Password: ", password);
+
+  const logIn = (userName, password) => {
+    return AuthService.login(userName, password)
+      .then((response) => {
+        console.log(response);
+        setPopUpBox("block");
+        // if (reload === "yes") {
+        //   setReload("no");
+        // window.location.reload();
+        // }
+        // redirectNow = "yes";
+        // window.location.reload();
+        // setRedirectNow("yes");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  // useEffect(() => {
+
+  // //   // AuthService.logout();
+  // //  logIn(userName, password);
+  //  console.log("logging");
+  // //   // window.location.reload();
+  // //   // setRedirectNow("yes");
+  // }, [userName, password]);
+
+  logIn(userName, password);
+  console.log("redirect: ", redirectNow);
+
+  const closePopUpBox = () => {
+    // redirectNow = "yes";
+    // setPopUpBox("none");
+    const path = generatePath(priorLocation);
+    console.log("Path: ", path);
+    const destination = history.push(path);
+    return destination;
+  };
 
   return (
     <>
       <div className={styles.formContainer}>Hello World!</div>
+      {/*
       {redirectNow ? (
-        <Route path="/*/demoLogin">
+        <Route path="/*/}
+      {/* demoLogin">
           <Redirect to={priorLocation} />
         </Route>
       ) : (
         <></>
       )}
+      */}
       <PopUpBoxComponent
         closePopUpBox={closePopUpBox}
         message={message}
