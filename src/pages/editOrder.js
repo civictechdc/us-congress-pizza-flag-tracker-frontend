f3c5a7fb6a8faf07bb0a44a1ae5ac7098134f42e
Merritt Baggett <mertbagt@gmail.com>
2022-07-29 08:50:49 -0400

    removed remaining invokations of LoginSubComponent

--- src/pages/editOrder.js
+++ src/pages/editOrder.js
@@ -3,22 +3,22 @@ import { Navigate, useNavigate, useParams } from "react-router-dom";
 
 import { editOrderControl, isUser } from "../components/permissions";
 import EditView from "../components/editView";
-import LoginSubComponent from "../components/loginSubComponent";
+import Login from "./login";
 import UserContext from "../components/userContext";
 
 const EditOrder = () => {
   const params = useParams("/orders/:id");
   let navigate = useNavigate();
-  const { setUserDisplay } = useContext(UserContext);  // rerenders when user logs in
+  const { setUserDisplay } = useContext(UserContext); // rerenders when user logs in
 
-  return isUser() ? (  
+  return isUser() ? (
     editOrderControl() ? (
       <EditView editId={params.id} navigate={navigate} />
     ) : (
       <Navigate to="/" replace />
     )
   ) : (
-    <LoginSubComponent />
+    <Login />
   );
 };
 
